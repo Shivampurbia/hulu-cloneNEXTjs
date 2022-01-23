5edb4d1e07c76e3dcff14210861971802f3ce346
Shivampurbia <shivam3265prb@gmail.com>
2022-01-23 20:01:05 +0530

    styling added

--- components/Result.js
+++ components/Result.js
@@ -3,7 +3,7 @@ import Thumbnail from './Thumbnail';
 import FlipMove from "react-flip-move";
 function Result({results}) {
     
-  return <FlipMove className='mt-4 px-4 my-8 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center'>
+  return <FlipMove className='mt-4 px-4 my-8 sm:grid md:grid-cols-2 xl:grid-cols-4 3xl:flex flex-wrap justify-center'>
       {
           results.map((result)=>(
               <Thumbnail key={result.id} result = {result }></Thumbnail>
